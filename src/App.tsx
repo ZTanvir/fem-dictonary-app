4d850f636fb9c860a111edc668feca9c4b2df303
zahirul <zahirulopel@gmail.com>
2026-03-15 16:57:19 +0600

    add loader screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
 import playAudioImg from "./assets/images/icon-play.svg";
 import SearchWordForm from "./components/SearchWordForm";
-import { useState } from "react";
 import type { ErrorMsgResponse } from "./utils/types";
+import Loading from "./components/Loading";
 
 const apiUrl = import.meta.env.VITE_DICTIONARY_API;
 
@@ -38,43 +39,58 @@ function App() {
     <>
       <Header />
       <SearchWordForm handleSearchWord={handleSearchWord} />
-      <section id="result">
-        <section id="phonetics" className="flex items-center">
-          <div className="mr-auto">
-            <h1 className="text-light-black text-5xl font-bold">Keyboard</h1>
-            <span className="text-fantasia mt-1 inline-block">/ˈkiːbɔːd/</span>
-          </div>
-          <div className="">
-            <audio
-              className=""
-              src="https://api.dictionaryapi.dev/media/pronunciations/en/keyboard-us.mp3"
-            >
-              Your browser does not support the audio element.
-            </audio>
-            <button>
-              <img src={playAudioImg} className="h-18 w-18" alt="play audio" />
-            </button>
-          </div>
-        </section>
 
-        <section id="partOfSpeech">
-          <h2>
-            noun <span></span>
-          </h2>
-          <p>Meaning</p>
-          <ul>
-            <li>
-              (etc.) A set of keys used to operate a typewriter, computer etc.
-            </li>
-            <li>
-              (etc.) A set of keys used to operate a typewriter, computer etc.
-            </li>
-            <li>
-              (etc.) A set of keys used to operate a typewriter, computer etc.
-            </li>
-          </ul>
+      {isLoading && (
+        <div className="flex h-full items-center justify-center">
+          <Loading />
+        </div>
+      )}
+
+      {wordData.length > 0 && (
+        <section id="result">
+          <section id="phonetics" className="flex items-center">
+            <div className="mr-auto">
+              <h1 className="text-light-black text-5xl font-bold">Keyboard</h1>
+              <span className="text-fantasia mt-1 inline-block">
+                /ˈkiːbɔːd/
+              </span>
+            </div>
+            <div className="">
+              <audio
+                className=""
+                src="https://api.dictionaryapi.dev/media/pronunciations/en/keyboard-us.mp3"
+              >
+                Your browser does not support the audio element.
+              </audio>
+              <button>
+                <img
+                  src={playAudioImg}
+                  className="h-18 w-18"
+                  alt="play audio"
+                />
+              </button>
+            </div>
+          </section>
+
+          <section id="partOfSpeech">
+            <h2>
+              noun <span></span>
+            </h2>
+            <p>Meaning</p>
+            <ul>
+              <li>
+                (etc.) A set of keys used to operate a typewriter, computer etc.
+              </li>
+              <li>
+                (etc.) A set of keys used to operate a typewriter, computer etc.
+              </li>
+              <li>
+                (etc.) A set of keys used to operate a typewriter, computer etc.
+              </li>
+            </ul>
+          </section>
         </section>
-      </section>
+      )}
     </>
   );
 }
---
+++ src/components/Loading.tsx
@@ -0,0 +1,5 @@
+export default function Loading() {
+  return (
+    <div className="border-t-fantasia h-10 w-10 animate-spin rounded-full border-4 border-gray-600" />
+  );
+}
