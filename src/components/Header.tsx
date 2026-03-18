26e70a092d96046d1250169088e54ca824a60163
zahirul <zahirulopel@gmail.com>
2026-03-18 17:23:16 +0600

    add non modal dialog for font selection

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,61 @@
 import LogoImg from "../assets/images/logo.svg";
 import MoonImg from "../assets/images/icon-moon.svg?react";
-import { useState } from "react";
+import DownArrowImg from "../assets/images/icon-arrow-down.svg?react";
+import { useRef, useState } from "react";
 import clsx from "clsx";
 
-export default function Header() {
+interface HeaderProps {
+  fontType: string;
+  setFontType: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Header({ fontType, setFontType }: HeaderProps) {
   const [mode, setMode] = useState("light");
+  const dialogEl = useRef<HTMLDialogElement>(null);
+
+  const handleOpenDialog = () => {
+    if (dialogEl.current?.open) {
+      dialogEl.current?.close();
+      return;
+    }
+    dialogEl.current?.show();
+  };
+  const handleCloseDialog = () => {
+    dialogEl.current?.close();
+  };
+
   return (
     <header className="flex items-center bg-white">
       <img className="mr-auto w-8" src={LogoImg} alt="Logo" />
       <div className="flex">
-        <select name="font" id="font">
-          <option value="sans">Sans Serif</option>
-        </select>
+        <div className="relative">
+          <button className="flex gap-2" onClick={handleOpenDialog}>
+            {fontType}
+            Python
+            <span className="self-center">
+              <DownArrowImg />
+            </span>
+          </button>
+          <dialog
+            className="left-auto z-1 w-40 rounded-xl py-4 shadow-sm"
+            ref={dialogEl}
+          >
+            <div className="flex flex-col space-y-3">
+              <button
+                onClick={handleCloseDialog}
+                className="pl-4 text-left font-sans hover:cursor-pointer"
+              >
+                Sans Serif
+              </button>
+              <button className="pl-4 text-left font-serif hover:cursor-pointer">
+                Serif
+              </button>
+              <button className="pl-4 text-left font-mono hover:cursor-pointer">
+                Mono
+              </button>
+            </div>
+          </dialog>
+        </div>
         <div className="bg-white-echo mx-4 h-auto w-px"></div>
         <div className="flex gap-3">
           <div
