66132379cdd744e64045770830e4a4e89d55a0eb
zahirul <zahirulopel@gmail.com>
2026-03-18 15:43:49 +0600

    add error msg when user want to submit empty form

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -6,7 +6,7 @@ interface ErrorMessageProps {
 }
 export default function ErrorMessage({ title, message }: ErrorMessageProps) {
   return (
-    <section className="flex flex-col items-center p-2">
+    <section className="mt-10 flex flex-col items-center p-2 sm:mt-35">
       <img src={EmojiImg} alt="sad emoji" />
       <h3 className="text-light-black mt-6 text-center text-lg font-bold">
         {title}
--- src/components/SearchWordForm.tsx
+++ src/components/SearchWordForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import SearchImg from "../assets/images/icon-search.svg";
+import clsx from "clsx";
 
 interface SearchWordFormProps {
   handleSearchWord: (word: string) => void;
@@ -9,22 +10,45 @@ export default function SearchWordForm({
   handleSearchWord,
 }: SearchWordFormProps) {
   const [word, setWord] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
   const handleSubmitForm = (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!word.length) {
+      setErrorMsg("Whoops, can't be empty...");
+      return;
+    }
     handleSearchWord(word);
+    setErrorMsg("");
   };
   return (
     <form onSubmit={handleSubmitForm} className="relative mt-10">
-      <input
-        value={word}
-        onChange={(e) => setWord(e.target.value)}
-        type="text"
-        name="search"
-        id="search"
-        className="bg-wild-sand text-light-black focus:ring-fantasia active:ring-fantasia caret-fantasia hover:ring-fantasia w-full rounded-2xl p-3 text-lg font-bold ring-0 transition duration-200 outline-none hover:ring-2 focus:ring-2 active:ring-1"
-        placeholder="Search for any word.."
-      />
-      <button type="submit" className="absolute top-1/2 right-3 -translate-1/2">
+      <div>
+        <input
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+          type="text"
+          name="search"
+          id="search"
+          className={clsx(
+            "bg-wild-sand text-light-black w-full rounded-2xl p-3 text-lg font-bold ring-0 transition duration-200 outline-none hover:ring-2 focus:ring-2 active:ring-1",
+
+            errorMsg
+              ? "focus:ring-hot-kiss active:ring-hot-kiss caret-fantasia hover:ring-hot-kiss"
+              : "focus:ring-fantasia active:ring-fantasia caret-fantasia hover:ring-fantasia",
+          )}
+          placeholder="Search for any word.."
+        />
+        {errorMsg && <p className="text-hot-kiss mt-2">{errorMsg}</p>}
+      </div>
+
+      <button
+        type="submit"
+        className={clsx(
+          "absolute right-3",
+          errorMsg ? "top-4" : "top-1/2 -translate-y-1/2",
+        )}
+      >
         <img src={SearchImg} alt="search icon" />
       </button>
     </form>
